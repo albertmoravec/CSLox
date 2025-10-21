a4e7a85bd0008ea99440b28d154c3b8d8db8398c
Albert Moravec <albert.moravec@gmail.com>
2025-10-21 23:03:54 +0200

    feat: Implement basic Lox scanner

---
+++ CSLox/Token.cs
@@ -0,0 +1,14 @@
+﻿namespace CSLox;
+
+public class Token(TokenType type, string lexeme, object? literal, int line)
+{
+    public TokenType Type { get; set; } = type;
+    public string Lexeme { get; set; } = lexeme;
+    public object Literal { get; set; } = literal;
+    public int Line { get; set; } = line;
+
+    public override string ToString()
+    {
+        return $"{Type} {Lexeme} {Literal}";
+    }
+}
