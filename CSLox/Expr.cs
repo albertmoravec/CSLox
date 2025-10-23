c8d691b33411fdebcfeb98bba46d24cee3abcca1
Albert Moravec <albert.moravec@gmail.com>
2025-10-23 09:12:18 +0200

    feat: Generate Expr node

---
+++ CSLox/Expr.cs
@@ -0,0 +1,48 @@
+namespace CSLox;
+
+public abstract class Expr {
+    public interface IVisitor<TR> {
+        TR VisitBinaryExpr(Binary expr);
+        TR VisitGroupingExpr(Grouping expr);
+        TR VisitLiteralExpr(Literal expr);
+        TR VisitUnaryExpr(Unary expr);
+    }
+
+    public class Binary(Expr left, Token oper, Expr right) : Expr {
+        public override TR Accept<TR>(IVisitor<TR> visitor)
+        {
+            return visitor.VisitBinaryExpr(this);
+        }
+
+        public Expr Left { get; set; } = left;
+        public Token Oper { get; set; } = oper;
+        public Expr Right { get; set; } = right;
+    }
+    public class Grouping(Expr expression) : Expr {
+        public override TR Accept<TR>(IVisitor<TR> visitor)
+        {
+            return visitor.VisitGroupingExpr(this);
+        }
+
+        public Expr Expression { get; set; } = expression;
+    }
+    public class Literal(object value) : Expr {
+        public override TR Accept<TR>(IVisitor<TR> visitor)
+        {
+            return visitor.VisitLiteralExpr(this);
+        }
+
+        public object Value { get; set; } = value;
+    }
+    public class Unary(Token oper, Expr right) : Expr {
+        public override TR Accept<TR>(IVisitor<TR> visitor)
+        {
+            return visitor.VisitUnaryExpr(this);
+        }
+
+        public Token Oper { get; set; } = oper;
+        public Expr Right { get; set; } = right;
+    }
+
+    public abstract TR Accept<TR>(IVisitor<TR> visitor);
+}
